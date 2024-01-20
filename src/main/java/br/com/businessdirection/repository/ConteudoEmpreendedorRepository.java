340c570f6f5f61a9ae94c97232c38f2ae95ead95
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-20 16:17:08 -0300

    Update ConteudoEmpreendedorRepository.java

--- src/main/java/br/com/businessdirection/repository/ConteudoEmpreendedorRepository.java
+++ src/main/java/br/com/businessdirection/repository/ConteudoEmpreendedorRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.businessdirection.model.ConteudoEmpreendedor;
 
@@ -13,8 +14,12 @@ public interface ConteudoEmpreendedorRepository extends JpaRepository<ConteudoEm
 	@EntityGraph(attributePaths = { "conteudoOnline", "empreendedor" })
 	List<ConteudoEmpreendedor> findAll();
 
-	@Query("SELECT ce FROM ConteudoEmpreendedor ce WHERE ce.ativo = true")
-	List<ConteudoEmpreendedor> findAllAtivos();
+	// listar todos os ativos usando query
+	@Query("SELECT ce FROM ConteudoEmpreendedor ce WHERE ce.ativo = :ativo")
+	List<ConteudoEmpreendedor> findAllAtivos(@Param("ativo") boolean ativo);
+
+	//listar todos os ativos usando jpa
+	List<ConteudoEmpreendedor> findAllConteudoEmpreendedor_Ativo(boolean ativo);
 
 	ConteudoEmpreendedor findByIdAndAtivo(Long id, boolean ativo);
 
