40887a978032c5aff0a9f252f91882694b318f81
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-21 00:50:57 -0300

    Update ModalidadeMentoriaController.java

--- src/main/java/br/com/businessdirection/controllers/ModalidadeMentoriaController.java
+++ src/main/java/br/com/businessdirection/controllers/ModalidadeMentoriaController.java
@@ -1,66 +1,65 @@
 package br.com.businessdirection.controllers;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.businessdirection.models.ModalidadeMentoria;
-import br.com.businessdirection.repositories.ModalidadeMentoriaRepository;
+import br.com.businessdirection.services.ModalidadeMentoriaService;
 
 @Controller
 @RequestMapping("/modalidades-mentorias")
 public class ModalidadeMentoriaController {
 
 	@Autowired
-	private ModalidadeMentoriaRepository modalidadeMentoriaRepository;
+	private ModalidadeMentoriaService service;
 
 	@GetMapping
-	public ModelAndView home() {
-		ModelAndView modelAndView = new ModelAndView("crudModalidadeMentoria/index");
-		modelAndView.addObject("modalidadesMentorias", modalidadeMentoriaRepository.findAll());
+	public ResponseEntity<List<ModalidadeMentoria>> findAll() {
+		List<ModalidadeMentoria> modalidadeMentorias = service.findAll();
 
-		return modelAndView;
+		return ResponseEntity.ok().body(modalidadeMentorias);
 	}
 
 	@GetMapping("/{id}")
-	public ModelAndView detalhes(@PathVariable Long id) {
-		ModelAndView modelAndView = new ModelAndView("crudModalidadeMentoria/detalhes");
-		modelAndView.addObject("modalidadeMentoria", modalidadeMentoriaRepository.getReferenceById(id));
+	public ResponseEntity<ModalidadeMentoria> findById(@PathVariable Long id) {
+		ModalidadeMentoria modalidadeMentoria = service.findById(id);
 
-		return modelAndView;
+		return ResponseEntity.ok().body(modalidadeMentoria);
 	}
 
-	@GetMapping("/cadastrar")
-	public ModelAndView cadastrar() {
-		ModelAndView modelAndView = new ModelAndView("crudModalidadeMentoria/formulario");
-		modelAndView.addObject("modalidadeMentoria", new ModalidadeMentoria());
-
-		return modelAndView;
+	@PostMapping
+	public ResponseEntity<Void> create(@RequestBody ModalidadeMentoria modalidadeMentoria) {
+		// UF estados = UF.values();
+		this.service.create(modalidadeMentoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(modalidadeMentoria.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
-	@GetMapping("/editar/{id}")
-	public ModelAndView editar(@PathVariable Long id) {
-		ModelAndView modelAndView = new ModelAndView("crudModalidadeMentoria/formulario");
-		modelAndView.addObject("modalidadeMentoria", modalidadeMentoriaRepository.getReferenceById(id));
+	@PutMapping("/editar/{id}")
+	public ResponseEntity<Void> update(@RequestBody ModalidadeMentoria modalidadeMentoria, @PathVariable Long id) {
+		modalidadeMentoria.setId(id);
+		this.service.update(modalidadeMentoria);
 
-		return modelAndView;
+		return ResponseEntity.noContent().build();
 	}
 
-	@PostMapping({ "/cadastrar", "/editar/{id}" })
-	public String salvar(ModalidadeMentoria modalidadeMentoria) {
-		modalidadeMentoriaRepository.save(modalidadeMentoria);
+	@DeleteMapping("/excluir/{id}")
+	public ResponseEntity<Void> excluir(@PathVariable Long id) {
+		service.delete(id);
 
-		return "redirect:/modalidades-mentorias";
-	}
-
-	@GetMapping("/excluir/{id}")
-	public String excluir(@PathVariable Long id) {
-		modalidadeMentoriaRepository.deleteById(id);
-
-		return "redirect:/modalidades-mentorias";
+		return ResponseEntity.noContent().build();
 	}
 }
