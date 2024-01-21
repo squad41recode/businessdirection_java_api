150666376c66bdaded5c016283caad3530d531c2
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-20 23:47:52 -0300

    Update ModalidadeMentoria.java

--- src/main/java/br/com/businessdirection/models/ModalidadeMentoria.java
+++ src/main/java/br/com/businessdirection/models/ModalidadeMentoria.java
@@ -1,6 +1,9 @@
 package br.com.businessdirection.models;
 
+import java.util.Objects;
+
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -15,12 +18,13 @@ public class ModalidadeMentoria {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(unique = true)
 	private Long id;
 
+	@Column(nullable = false, length = 80, unique = true)
 	private String nomeModalidade;
 
-	@OneToOne(mappedBy = "modalidadeMentoria", cascade = CascadeType.ALL, fetch = FetchType.LAZY) // , orphanRemoval =
-																									// true
+	@OneToOne(mappedBy = "modalidadeMentoria", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private ConteudoOnline conteudoOnline;
 
 //	talvez criar um metodo que calcula a qtd e passa o valor pra ca
@@ -31,11 +35,12 @@ public class ModalidadeMentoria {
 	public ModalidadeMentoria() {
 		super();
 	}
-
-	public ModalidadeMentoria(Long id, String nomeModalidade) {
+	
+	public ModalidadeMentoria(Long id, String nomeModalidade, ConteudoOnline conteudoOnline) {
 		super();
 		this.id = id;
 		this.nomeModalidade = nomeModalidade;
+		this.conteudoOnline = conteudoOnline;
 	}
 
 	public Long getId() {
@@ -53,5 +58,33 @@ public class ModalidadeMentoria {
 	public void setNomeModalidade(String nomeModalidade) {
 		this.nomeModalidade = nomeModalidade;
 	}
+
+	public ConteudoOnline getConteudoOnline() {
+		return conteudoOnline;
+	}
+
+	public void setConteudoOnline(ConteudoOnline conteudoOnline) {
+		this.conteudoOnline = conteudoOnline;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(conteudoOnline, id, nomeModalidade);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ModalidadeMentoria other = (ModalidadeMentoria) obj;
+		return Objects.equals(conteudoOnline, other.conteudoOnline) && Objects.equals(id, other.id)
+				&& Objects.equals(nomeModalidade, other.nomeModalidade);
+	}
+
+
 
 }
