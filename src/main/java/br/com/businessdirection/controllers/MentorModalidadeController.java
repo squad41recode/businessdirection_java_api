b484e754c2e4c820a49c7136d036ad77f4c050a1
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-22 16:27:22 -0300

    Update MentorModalidadeController.java

--- src/main/java/br/com/businessdirection/controllers/MentorModalidadeController.java
+++ src/main/java/br/com/businessdirection/controllers/MentorModalidadeController.java
@@ -1,86 +1,66 @@
 package br.com.businessdirection.controllers;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.businessdirection.models.MentorModalidade;
-import br.com.businessdirection.repositories.MentorModalidadeRepository;
-import br.com.businessdirection.repositories.MentorRepository;
-import br.com.businessdirection.repositories.ModalidadeMentoriaRepository;
+import br.com.businessdirection.services.MentorModalidadeService;
 
-@Controller
-@RequestMapping("/mentorias-disponiveis")
+@RestController
+@RequestMapping("/api/mentorias-disponiveis")
 public class MentorModalidadeController {
 
 	@Autowired
-	private MentorModalidadeRepository mentorModalidadeRepository;
-
-	@Autowired
-	private MentorRepository mentorRepository;
-
-	@Autowired
-	private ModalidadeMentoriaRepository modalidadeMentoriaRepository;
+	private MentorModalidadeService mentorModalidadeService;
 
 	@GetMapping
-	public ModelAndView home() {
-		ModelAndView modelAndView = new ModelAndView("crudMentorModalidade/index");
-		modelAndView.addObject("MentorModalidades", mentorModalidadeRepository.findAll());
-
-		return modelAndView;
+	public ResponseEntity<List<MentorModalidade>> findAll() {
+		List<MentorModalidade> conteudosOnline = mentorModalidadeService.findAll();
+		return ResponseEntity.ok().body(conteudosOnline);
 	}
 
 	@GetMapping("/{id}")
-	public ModelAndView detalhes(@PathVariable Long id) {
-		ModelAndView modelAndView = new ModelAndView("crudMentorModalidade/detalhes");
-		modelAndView.addObject("mentorModalidade", mentorModalidadeRepository.findById(id));
-
-		return modelAndView;
+	public ResponseEntity<MentorModalidade> findById(@PathVariable Long id) {
+		MentorModalidade mentorModalidade = mentorModalidadeService.findById(id);
+		return ResponseEntity.ok().body(mentorModalidade);
 	}
 
-	@GetMapping("/cadastrar")
-	public ModelAndView cadastrar() {
-		ModelAndView modelAndView = new ModelAndView("crudMentorModalidade/formulario");
-		modelAndView.addObject("mentorModalidade", new MentorModalidade());
-		modelAndView.addObject("mentores", mentorRepository.findAll());
-		modelAndView.addObject("modalidades", modalidadeMentoriaRepository.findAll());
+	@PostMapping
+	public ResponseEntity<Void> create(@RequestBody MentorModalidade mentorModalidade) {
+		// List<ModalidadeMentoria> modalidades = modalidadeMentoriaService.findAll();
+		// mentorModalidade.setModalidadeMentoria(modalidades);
+		mentorModalidadeService.create(mentorModalidade, 
+				mentorModalidade.getMentor().getId(),
+				mentorModalidade.getModalidadeMentoria().getId());
 
-		return modelAndView;
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(mentorModalidade.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 
-	@GetMapping("/editar/{id}")
-	public ModelAndView editar(@PathVariable Long id) {
-		ModelAndView modelAndView = new ModelAndView("crudMentorModalidade/formulario");
-		modelAndView.addObject("mentorModalidade", mentorModalidadeRepository.getReferenceById(id));
-		modelAndView.addObject("mentores", mentorRepository.findAll());
-		modelAndView.addObject("modalidades", modalidadeMentoriaRepository.findAll());
-
-		return modelAndView;
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@RequestBody MentorModalidade mentorModalidade, @PathVariable Long id) {
+		mentorModalidade.setId(id);
+		mentorModalidadeService.update(mentorModalidade);
+		return ResponseEntity.noContent().build();
 	}
 
-	@PostMapping({ "/cadastrar", "/editar/{id}" })
-	public String salvar(MentorModalidade mentorModalidade) {
-		mentorModalidade.setAtivo(true);
-		mentorModalidadeRepository.save(mentorModalidade);
-
-		return "redirect:/mentorias-disponiveis";
-	}
-
-	@GetMapping("/excluir/{id}")
-	public String excluir(@PathVariable Long id) {
-		mentorModalidadeRepository.deleteById(id);
-		/*
-		 * Optional<MentorModalidade> mentorModalidadeOptional =
-		 * mentorModalidadeRepository.findById(id);
-		 * mentorModalidadeOptional.ifPresent(mentorModalidade -> {
-		 * mentorModalidade.setAtivo(false);
-		 * mentorModalidadeRepository.save(mentorModalidade); });
-		 */
-
-		return "redirect:/mentorias-disponiveis";
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> excluir(@PathVariable Long id) {
+		mentorModalidadeService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
