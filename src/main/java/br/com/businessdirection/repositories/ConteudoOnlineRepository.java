edcf656f64e342c7462868ccf8d1c52477719b91
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-22 16:25:36 -0300

    Update ConteudoOnlineRepository.java

--- src/main/java/br/com/businessdirection/repositories/ConteudoOnlineRepository.java
+++ src/main/java/br/com/businessdirection/repositories/ConteudoOnlineRepository.java
@@ -1,6 +1,7 @@
 package br.com.businessdirection.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,9 @@ public interface ConteudoOnlineRepository extends JpaRepository<ConteudoOnline, 
 
 	@EntityGraph(attributePaths = { "modalidadeMentoria" })
 	List<ConteudoOnline> findAll();
+
+	// @Query("SELECT co FROM ConteudoOnline co WHERE co.id = :id")
+	@EntityGraph(attributePaths = { "modalidadeMentoria" })
+	Optional<ConteudoOnline> findById(Long id);
+
 }
