c323a6f669b204a1f2b3ed3a28039dff0febcc79
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-22 16:27:40 -0300

    Update ConteudoEmpreendedorController.java

--- src/main/java/br/com/businessdirection/controllers/ConteudoEmpreendedorController.java
+++ src/main/java/br/com/businessdirection/controllers/ConteudoEmpreendedorController.java
@@ -1,91 +1,63 @@
 package br.com.businessdirection.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.businessdirection.models.ConteudoEmpreendedor;
-import br.com.businessdirection.models.ConteudoOnline;
-import br.com.businessdirection.models.Empreendedor;
-import br.com.businessdirection.repositories.ConteudoEmpreendedorRepository;
-import br.com.businessdirection.repositories.ConteudoOnlineRepository;
-import br.com.businessdirection.repositories.EmpreendedorRepository;
+import br.com.businessdirection.services.ConteudoEmpreendedorService;
 
 @Controller
-@RequestMapping("/conteudo-estudado")
+@RequestMapping("/api/conteudos-estudados")
 public class ConteudoEmpreendedorController {
 
 	@Autowired
-	private ConteudoOnlineRepository conteudoOnlineRepository;
-
-	@Autowired
-	private EmpreendedorRepository empreendedorRepository;
-
-	@Autowired
-	private ConteudoEmpreendedorRepository conteudoEmpreendedorRepository;
+	private ConteudoEmpreendedorService conteudoEmpreendedorService;
 
 	@GetMapping
-	public ModelAndView home() {
-		ModelAndView modelAndView = new ModelAndView("crudConteudoEmpreendedor/index");
-		modelAndView.addObject("conteudosEmpreendedor", conteudoEmpreendedorRepository.findAll());
-
-		return modelAndView;
+	public ResponseEntity<List<ConteudoEmpreendedor>> findAll() {
+		List<ConteudoEmpreendedor> conteudosOnline = conteudoEmpreendedorService.findAll();
+		return ResponseEntity.ok().body(conteudosOnline);
 	}
 
 	@GetMapping("/{id}")
-	public ModelAndView detalhes(@PathVariable Long id) {
-		ModelAndView modelAndView = new ModelAndView("crudConteudoEmpreendedor/detalhes");
-		modelAndView.addObject("conteudoEmpreendedor", conteudoEmpreendedorRepository.findById(id));
-
-		return modelAndView;
+	public ResponseEntity<ConteudoEmpreendedor> findById(@PathVariable Long id) {
+		ConteudoEmpreendedor conteudoEmpreendedor = conteudoEmpreendedorService.findById(id);
+		return ResponseEntity.ok().body(conteudoEmpreendedor);
 	}
 
-	@GetMapping("/cadastrar")
-	public ModelAndView cadastrar() {
-		ModelAndView modelAndView = new ModelAndView("crudConteudoEmpreendedor/formulario");
+	@PostMapping
+	public ResponseEntity<Void> create(@RequestBody ConteudoEmpreendedor conteudoEmpreendedor) {
+		conteudoEmpreendedorService.create(conteudoEmpreendedor, conteudoEmpreendedor.getEmpreendedor().getId(),
+				conteudoEmpreendedor.getConteudoOnline().getId());
 
-		List<Empreendedor> empreendedores = empreendedorRepository.findAll();
-		List<ConteudoOnline> conteudosOnline = conteudoOnlineRepository.findAll();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(conteudoEmpreendedor.getId()).toUri();
 
-		modelAndView.addObject("conteudosOnline", conteudosOnline);
-		modelAndView.addObject("empreendedores", empreendedores);
-		modelAndView.addObject("conteudoEmpreendedor", new ConteudoEmpreendedor());
-
-		return modelAndView;
+		return ResponseEntity.created(uri).build();
 	}
 
-	@GetMapping("/editar/{id}")
-	public ModelAndView editar(@PathVariable Long id) {
-		ModelAndView modelAndView = new ModelAndView("crudConteudoEmpreendedor/formulario");
-
-		List<Empreendedor> empreendedores = empreendedorRepository.findAll();
-		List<ConteudoOnline> conteudosOnline = conteudoOnlineRepository.findAll();
-
-		modelAndView.addObject("conteudosOnline", conteudosOnline);
-		modelAndView.addObject("empreendedores", empreendedores);
-		modelAndView.addObject("conteudoEmpreendedor", conteudoEmpreendedorRepository.getReferenceById(id));
-
-		return modelAndView;
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@RequestBody ConteudoEmpreendedor conteudoEmpreendedor, @PathVariable Long id) {
+		conteudoEmpreendedor.setId(id);
+		conteudoEmpreendedorService.update(conteudoEmpreendedor);
+		return ResponseEntity.noContent().build();
 	}
 
-	@PostMapping({ "/cadastrar", "/editar/{id}" })
-	public String salvar(ConteudoEmpreendedor conteudoEmpreendedor) {
-		conteudoEmpreendedor.setAtivo(true);
-		conteudoEmpreendedorRepository.save(conteudoEmpreendedor);
-
-		return "redirect:/conteudo-estudado";
-	}
-
-	@GetMapping("/excluir/{id}")
-	public String excluir(@PathVariable Long id) {
-		conteudoEmpreendedorRepository.deleteById(id);
-
-		return "redirect:/conteudo-estudado";
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> excluir(@PathVariable Long id) {
+		conteudoEmpreendedorService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
