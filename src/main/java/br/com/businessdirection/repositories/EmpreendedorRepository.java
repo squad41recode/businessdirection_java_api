5a68293e9f7dda4d1a01e7fc7ef7a8e303a66591
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-22 16:25:05 -0300

    Update EmpreendedorRepository.java

--- src/main/java/br/com/businessdirection/repositories/EmpreendedorRepository.java
+++ src/main/java/br/com/businessdirection/repositories/EmpreendedorRepository.java
@@ -10,6 +10,19 @@ import br.com.businessdirection.models.Empreendedor;
 public interface EmpreendedorRepository extends JpaRepository<Empreendedor, Long> {
 
 	@EntityGraph(attributePaths = { "mentoriasAdquiridas" })
-	List<Empreendedor> findAll();// , "conteudosEstudados"
+	List<Empreendedor> findAllByMentoriasAdquiridasIsNotNull();// , "conteudosEstudados"
+	// List<Empreendedor> findAllById(Long id);//
+
+	@EntityGraph(attributePaths = { "conteudosEstudados" })
+	List<Empreendedor> findAllByConteudosEstudadosIsNotNull();
+	/*
+	 * @Query("SELECT e FROM Empreendedor e JOIN FETCH e.conteudosEstudados ce WHERE ce IS NOT NULL"
+	 * ) List<Empreendedor> findAllConteudosEstudados();//
+	 */
+	/*
+	 * @EntityGraph(attributePaths = { "mentoriasAdquiridas", "conteudosEstudados"
+	 * }) List<Empreendedor> findAll();//
+	 */
+	// List<ConteudoEmpreendedor> findAllConteudosEstudados();
 
 }
