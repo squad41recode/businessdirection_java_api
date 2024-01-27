6075bfe8c48b35e3a2eabf0e71324fc98f84c07c
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-01-27 03:22:17 -0300

    Update ModalidadeMentoriaRepository.java

--- src/main/java/br/com/businessdirection/repositories/ModalidadeMentoriaRepository.java
+++ src/main/java/br/com/businessdirection/repositories/ModalidadeMentoriaRepository.java
@@ -1,9 +1,14 @@
 package br.com.businessdirection.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.businessdirection.models.ModalidadeMentoria;
 
 public interface ModalidadeMentoriaRepository extends JpaRepository<ModalidadeMentoria, Long> {
 
+	@Query("SELECT mm FROM ModalidadeMentoria mm WHERE NOT EXISTS (SELECT co FROM ConteudoOnline co WHERE mm.id = co.modalidadeMentoria.id)")
+	List<ModalidadeMentoria> listarModalidadesSemLigacaoComConteudos();
 }
